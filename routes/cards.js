7417302693e7f1f788ff66bcc62db6a5d0056a72
romans1995 <cboby26@gmail.com>
2023-02-03 17:21:11 +0200

    fix all linter errors

--- routes/cards.js
+++ routes/cards.js
@@ -1,20 +1,17 @@
 const router = require('express').Router();
 const cards = require('../data/cards.json');
 
-
 router.get('/cards', (req, res) => {
-    res.send('cards');
+  res.send('cards');
 });
 router.get('/cards/:id', (req, res) => {
-    const { id } = req.params;
-    const resolt = cards.find((card) => card._id === id);
-    if (resolt === undefined) {
-        res.send({ error: `This user doesn't exist` });
-        return;
-    }
-    res.send(resolt);
-    console.log(resolt);
+  const { id } = req.params;
+  const resolt = cards.find((card) => card._id === id);
+  if (resolt === undefined) {
+    res.send({ error: "This user doesn't exist" });
+    return;
+  }
+  res.send(resolt);
 });
-
 
 module.exports = router;
