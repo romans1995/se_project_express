3368466fc7387bea911c08ab297b1257f1ce16e8
romans1995 <cboby26@gmail.com>
2023-02-09 19:02:59 +0200

    fixed:routes logic

--- routes/cards.js
+++ routes/cards.js
@@ -2,23 +2,25 @@ const router = require('express').Router();
 const fs = require('fs').promises;
 const path = require('path');
 
-let cardsData = [];
 const filePath = path.resolve('./data/');
+
 router.get('/', (req, res) => {
   fs.readFile(`${filePath}/cards.json`, 'utf-8')
     .then((data) => {
-      cardsData = JSON.parse(data);
-      res.send(cardsData);
+      res.send(JSON.parse(data));
     })
     .catch(() => res.status(500).send({ message: 'something went wrong' }));
 });
+
 router.get('/:id', (req, res) => {
-  const { id } = req.params;
-  const resolt = cardsData.find((user) => user._id === id);
-  if (resolt === undefined) {
-    res.status(404).send({ message: "This user doesn't exist" });
-    return;
-  }
-  res.send(resolt);
+  fs.readFile(`${filePath}/cards.json`, 'utf-8').then((data) => {
+    const { id } = req.params;
+    const resolt = JSON.parse(data).find((card) => card._id === id);
+    if (resolt === undefined) {
+      res.status(404).send({ message: "This user doesn't exist" });
+      return;
+    }
+    res.send(resolt);
+  }).catch(() => res.status(500).send({ message: 'something went wrong' }));
 });
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,23 +2,25 @@ const router = require('express').Router();
 const fs = require('fs').promises;
 const path = require('path');
 
-let usersData = [];
 const filePath = path.resolve('./data/');
+
 router.get('/', (req, res) => {
   fs.readFile(`${filePath}/users.json`, 'utf-8')
     .then((data) => {
-      usersData = JSON.parse(data);
-      res.send(usersData);
+      res.send(JSON.parse(data));
     })
     .catch(() => res.status(500).send({ message: 'something went wrong' }));
 });
+
 router.get('/:id', (req, res) => {
-  const { id } = req.params;
-  const resolt = usersData.find((user) => user._id === id);
-  if (resolt === undefined) {
-    res.status(404).send({ message: "This user doesn't exist" });
-    return;
-  }
-  res.send(resolt);
+  fs.readFile(`${filePath}/users.json`, 'utf-8').then((data) => {
+    const { id } = req.params;
+    const resolt = JSON.parse(data).find((user) => user._id === id);
+    if (resolt === undefined) {
+      res.status(404).send({ message: "This user doesn't exist" });
+      return;
+    }
+    res.send(resolt);
+  }).catch(() => res.status(500).send({ message: 'something went wrong' }));
 });
 module.exports = router;
